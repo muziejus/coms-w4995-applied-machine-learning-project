679c329a8c3de9fc17b8a886257ef2abaae24a6a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2024-12-06 15:41:59 -0500

    feat: Add pickling logic.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ by Ruibin Lyu, Vi Mai, Julie Meunier, Tuba Opel, Moacir P. de Sá Pereira
 """
 
 import sys
+import pickle
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,12 +16,15 @@ from sentiment import get_daily_sentiment
 from expand_financial_data import expand_financial_data
 from eda import eda
 from models.random_forest_classifier import random_forest_classifier
+from models.svm import svm
 
 companies = ["dltr", "lulu", "ulta", "wba", "wmt"]
 
 start_date = "2019-01-01"
 end_date = "2024-10-16"
 date_range = pd.date_range(start=start_date, end=end_date, freq="D")
+
+rerun = False
 
 
 def collect_data():
@@ -71,80 +76,47 @@ def print_eda():
     print(eda_df)
 
 
+# LSTM model
+def lstm_model():
+    if rerun:
+        results = []
+        for company in companies:
+            cm, accuracy, f1score = lstm(company)
+            results.append((company, cm, accuracy, f1score))
+        with open("models/lstm_results.pkl", "wb") as f:
+            pickle.dump(results, f)
+
+
 # SVM Model
+def svm_model():
+    if rerun:
+        results = []
+        for company in companies:
+            cm, accuracy, f1score = svm(company)
+            results.append((company, cm, accuracy, f1score))
+        with open("models/svm_results.pkl", "wb") as f:
+            pickle.dump(results, f)
 
 
 # Random Forest model
 def random_forest():
-    results = []
-    for company in companies:
-        confusion_matrix_display, features, importances = random_forest_classifier(
-            company
-        )
-        results.append((company, confusion_matrix_display, features, importances))
-
-    # Plot using subplots
-    fig, axes = plt.subplots(2, 5, figsize=(14, 8), constrained_layout=True)
-    plt.tight_layout()
-
-    for i, (company, confusion_matrix_display, features, importances) in enumerate(
-        results
-    ):
-        confusion_matrix_display.plot(ax=axes[0, i])
-        confusion_matrix_display.ax_.set_title(
-            f"{company.upper()} Confusion Matrix", fontsize=12
-        )
-        confusion_matrix_display.im_.colorbar.remove()
-        confusion_matrix_display.ax_.set_xlabel("")
-        # cm_ax.set_title()
-        # cm_ax.set_xlabel("Predicted")
-        # cm_ax.set_ylabel("Actual")
-
-        # Feature Importance subplot (bottom row)
-        # sorted_feats_imps = sorted(zip(feats, imps), key=lambda x: x[1], reverse=True)
-        # top_feats, top_imps = zip(*sorted_feats_imps[:10])  # Show top 10 features
-        # wrapped_feats = ["\n".join(textwrap.wrap(f, width=15)) for f in top_feats]
-        ax = sns.barplot(y=list(importances), x=list(features), ax=axes[1, i])
-        # ax.set_titleax.tick_params(axis="x", rotation=45)  # Rotate x-axis labels by 45 degrees
-        ax.tick_params(axis="x", rotation=90)
-        ax.set_xticklabels(ax.get_xticklabels(), fontsize=10)
-        #     f"{company.upper()} Feature Selection Based on Random Forest Classification"
-        # )
-        ax.set_ylabel("Importance")
-        ax.set_xlabel("Features")
-
-    plt.suptitle(
-        "Confusion Matrices and Feature Importances for Companies", fontsize=16
-    )
-    plt.savefig(
-        "plots/random_forest/faceted_random_forest.png",
-        dpi=300,
-        bbox_inches="tight",
-    )
-
-
-# plt.savefig(
-#     f"plots/random_forest/{company}_confusion_matrix.png",
-#     dpi=300,
-#     bbox_inches="tight",
-# )
-# plt.figure(figsize=(12, 10))
-# plt.tight_layout()
-# ax = sns.barplot(x=list(imps), y=list(feats))
-# ax.set_title(
-#     f"{company.upper()} Feature Selection Based on Random Forest Classification"
-# )
-# ax.set_xlabel("Importance")
-# ax.set_ylabel("Features")
-# plt.savefig(
-#     f"plots/random_forest/{company}_feature_selection.png",
-#     dpi=300,
-#     bbox_inches="tight",
-# )
+    if rerun:
+        results = []
+        for company in companies:
+            cm, accuracy, f1score = random_forest_classifier(company)
+            results.append((company, cm, accuracy, f1score))
+        with open("models/random_forest_results.pkl", "wb") as f:
+            pickle.dump(results, f)
+    else:
+        with open("models/random_forest_results.pkl", "rb") as f:
+            results = pickle.load(f)
+            print(results[0])
 
 
 if __name__ == "__main__":
     # Check if an argument is passed
+    if len(sys.argv) > 2 and sys.argv[2] == "rerun":
+        rerun = True
     if len(sys.argv) > 1:
         argument = sys.argv[1]
         if argument == "collect_data":
@@ -153,6 +125,10 @@ if __name__ == "__main__":
             print_eda()
         elif argument == "random_forest":
             random_forest()
+        elif argument == "svm":
+            svm_model()
+        elif argument == "lstm":
+            lstm_model()
         else:
             print("Invalid argument. Please use 'collect_data'.")
     else:
